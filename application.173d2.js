4bd6d9cde17b40ff1835cd95b5836f81fd4cd33f
tellerYami <>
2026-04-30 16:32:42 +0800

    1

--- application.173d2.js
+++ application.173d2.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.93ebe.json';
+          this.settingsPath = 'src/settings.38f5b.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.c3709.js
+++ index.c3709.js
@@ -1,4 +1,4 @@
-System.register(["./application.1f88f.js"], function (_export, _context) {
+System.register(["./application.173d2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
