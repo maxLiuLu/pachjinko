18e34214d85dcbd0fd65eebdf80616e478547438
tellerYami <>
2026-05-01 14:11:41 +0800

    1

--- index.46055.js
+++ index.46055.js
@@ -1,4 +1,4 @@
-System.register(["./application.66585.js"], function (_export, _context) {
+System.register(["./application.ab18a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
